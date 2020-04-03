3d592eae4ab271ce13423347d1763e69931071ce
Jacob Robinson <jrobins6@ncsu.edu>
2020-04-03 15:41:52 -0400

    added test folder

---
+++ Project2/test/edu/ncsu/csc216/business/model/stakeholders/PropertyManagerTest.java
@@ -0,0 +1,14 @@
+package edu.ncsu.csc216.business.model.stakeholders;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PropertyManagerTest {
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+	}
+
+}

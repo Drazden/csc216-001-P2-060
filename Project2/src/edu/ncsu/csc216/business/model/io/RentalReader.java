eb345d3105bdfd5db0047ac4e818325bfb789a27
Jacob Robinson <jrobins6@ncsu.edu>
2020-04-03 10:21:08 -0400

    Added util classes. Created IO classes skeletons

---
+++ Project2/src/edu/ncsu/csc216/business/model/io/RentalReader.java
@@ -0,0 +1,21 @@
+package edu.ncsu.csc216.business.model.io;
+
+/**
+ * RentalReader reads data from text files. It has one static method, readRentalData. 
+ * @author Jacob Robinson
+ *
+ */
+public class RentalReader {
+
+	/**
+	 * readRentalData reads data from a text file. The parameter is the name of the file to read from.
+	 * If any errors occur during reading, all data is cleared and an exception is thrown.
+	 * This method is static to allow calling from other classes.
+	 * @param filename String containing name of file to read
+	 * @throws IllegalArgumentException exception thrown if any errors occur
+	 */
+	public static void readRentalData(String filename) {
+		//Not yet implemented
+	}
+	
+}
---
+++ Project2/src/edu/ncsu/csc216/business/model/io/RentalWriter.java
@@ -0,0 +1,21 @@
+package edu.ncsu.csc216.business.model.io;
+
+/**
+ * RentalWriter writes data to text files. It has one static method, writeentalData. 
+ * @author Jacob Robinson
+ *
+ */
+public class RentalWriter {
+
+	/**
+	 * writeRentalData writes data to a text file. The parameter is the name of the file to write to.
+	 * If any errors occur during writing, an exception is thrown.
+	 * This method is static to allow calling from other classes.
+	 * @param filename String containing name of file to write
+	 * @throws IllegalArgumentException exception thrown if any errors occur
+	 */
+	public static void writeRentalData(String filename) {
+		//Not yet implemented
+	}
+	
+}

1731d9ee342ce4f2168ff5f1488e984c3f15de63
Jacob Robinson <jrobins6@ncsu.edu>
2020-04-03 14:55:02 -0400

    added duplicate exceptions

--- Project2/src/edu/ncsu/csc216/business/model/stakeholders/DuplicateClientException.java
+++ Project2/src/edu/ncsu/csc216/business/model/stakeholders/DuplicateClientException.java
@@ -1,5 +1,29 @@
 package edu.ncsu.csc216.business.model.stakeholders;
 
-public class DuplicateClientException {
+/**
+ * Class for exception thrown when client already exists
+ * @author Jacob Robinson
+ *
+ */
+public class DuplicateClientException extends Exception {
 
+	/**
+	 * ID used for serialization
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Constructs with a custom message
+	 * @param message custom string
+	 */
+	public DuplicateClientException(String message) {
+		super(message);
+	}
+	
+	/**
+	 * Constructs with default message
+	 */
+	public DuplicateClientException() {
+		this("Client already exists");
+	}
 }
--- Project2/src/edu/ncsu/csc216/business/model/stakeholders/DuplicateRoomException.java
+++ Project2/src/edu/ncsu/csc216/business/model/stakeholders/DuplicateRoomException.java
@@ -1,5 +1,29 @@
 package edu.ncsu.csc216.business.model.stakeholders;
 
-public class DuplicateRoomException {
+/**
+ * Class for exception thrown when room already exists
+ * @author Jacob Robinson
+ *
+ */
+public class DuplicateRoomException extends Exception {
 
+	/**
+	 * ID used for serialization
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Constructs with a custom message
+	 * @param message custom string
+	 */
+	public DuplicateRoomException(String message) {
+		super(message);
+	}
+	
+	/**
+	 * Constructs with default message
+	 */
+	public DuplicateRoomException() {
+		this("Room already exists");
+	}
 }

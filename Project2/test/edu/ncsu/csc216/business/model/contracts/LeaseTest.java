5202e2f1da8294e91f831064cc43795014198828
Jacob Robinson <jrobins6@ncsu.edu>
2020-04-17 12:55:27 -0400

    updated leastest for more coverage

--- Project2/test/edu/ncsu/csc216/business/model/contracts/LeaseTest.java
+++ Project2/test/edu/ncsu/csc216/business/model/contracts/LeaseTest.java
@@ -7,6 +7,7 @@ import java.time.LocalDate;
 import org.junit.Test;
 
 import edu.ncsu.csc216.business.model.properties.ConferenceRoom;
+import edu.ncsu.csc216.business.model.properties.HotelSuite;
 import edu.ncsu.csc216.business.model.properties.RentalUnit;
 import edu.ncsu.csc216.business.model.stakeholders.Client;
 
@@ -29,6 +30,12 @@ public class LeaseTest {
 		
 		Lease lease = new Lease(0, client, unit, start, end, 1);
 		
+		HotelSuite suite = new HotelSuite("10-10", 1);
+		
+		Lease leaseS = new Lease(1, client, suite, start, end, 1);
+		String[] suiteData = leaseS.leaseData();
+		assertEquals("Hotel Suite:10-10", suiteData[3]);
+		
 		assertEquals(0, lease.getConfirmationNumber());
 		assertEquals(client, lease.getClient());
 		assertEquals(unit, lease.getProperty());
@@ -40,7 +47,7 @@ public class LeaseTest {
 		assertEquals("000000", data[0]);
 		
 		Lease lease2 = new Lease(client, unit, start, end, 1);
-		assertEquals(1, lease2.getConfirmationNumber());
+		assertEquals(2, lease2.getConfirmationNumber());
 		
 		LocalDate endNew = LocalDate.of(2020, 4, 16);
 		lease.setEndDateEarlier(endNew);
@@ -48,6 +55,7 @@ public class LeaseTest {
 		
 		
 		assertEquals(-1, lease.compareTo(lease2));
+		assertEquals(-1, lease.compareTo(leaseS));
 	}
 
 }
